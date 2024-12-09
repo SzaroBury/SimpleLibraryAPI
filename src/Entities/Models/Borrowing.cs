a8823b5a84a0e4b9db843147e7731339f7d900f7
SzaroBury <mkmateuszk@o2.pl>
2024-12-09 17:24:13 +0100

    ActualReturnDate will be null by default

--- src/Entities/Models/Borrowing.cs
+++ src/Entities/Models/Borrowing.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public DateTime StartedDate { get; set; }
         public DateTime ExpectedReturnDate => StartedDate.AddDays(14);  
-        public DateTime? ActualReturnDate { get; set; }
+        public DateTime? ActualReturnDate { get; set; } = null;
         public int CopyId { get; set; }
         public Copy? Copy { get; set; }
         public int ReaderId { get; set; }
