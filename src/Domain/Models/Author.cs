2e80df0fe52bfc26b15ceea5b2685eacdd5e3228
SzaroBurys <mkmateuszk@o2.pl>
2025-03-27 15:28:39 +0100

    Changed ids in model from int to string

--- src/Domain/Models/Author.cs
+++ src/Domain/Models/Author.cs
@@ -1,11 +1,15 @@
-﻿namespace SimpleLibrary.Domain.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleLibrary.Domain.Models;
 
 public class Author
 {
-    public int Id { get; set; }
-    public string FirstName { get; set; } = "";
-    public string LastName { get; set; } = "";
-    public string Description { get; set; } = "";
+    public Guid Id { get; set; } = Guid.NewGuid();
+    [Required] public string FirstName { get; set; } = string.Empty;
+    [Required] public string LastName { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public DateTime? BornDate { get; set; }
-    public string Tags { get; set; } = "";
+    public string Tags { get; set; } = string.Empty;
+
+    public virtual ICollection<Book> Books { get; set; } = [];
 }
--- src/Domain/Models/Book.cs
+++ src/Domain/Models/Book.cs
@@ -6,18 +6,18 @@ namespace SimpleLibrary.Domain.Models;
 
 public class Book
 {
-    public int Id { get; set; }
-    public string Title { get; set; } = null!;
-    public string Description { get; set; } = "";
-    [Required]
-    public DateTime? ReleaseDate { get; set; }
-    [Required]
-    public Language? Language { get; set; } 
-    public string Tags { get; set; } = "";
+    public Guid Id { get; set; } = Guid.NewGuid();
+    [Required] public required string Title { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public DateTime ReleaseDate { get; set; }
+    public Language Language { get; set; } 
+    public string Tags { get; set; } = string.Empty;
 
-    public int AuthorId { get; set; } = 0;
-    public Author? Author { get; set; }
-    public int CategoryId { get; set; } = 0;
-    public Category? Category { get; set; }
+    [Required] public required virtual Author Author { get; set; }
+    public Guid AuthorId { get; set; }
+    
+    [Required] public required virtual Category Category { get; set; }
+    public Guid CategoryId { get; set; }
 
+    public virtual ICollection<Copy> Copies { get; set; } = [];
 }
