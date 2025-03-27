a4a3907fa2ec882d60f7cc55d1480b63e7b39621
SzaroBurys <mkmateuszk@o2.pl>
2025-03-27 15:28:39 +0100

    Changed type of IDs in model classes from integers to GUIDS

--- src/Domain/Models/Reader.cs
+++ src/Domain/Models/Reader.cs
@@ -1,14 +1,19 @@
-﻿using SimpleLibrary.Domain.Enumerations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace SimpleLibrary.Domain.Models;
 
 public class Reader
 {
-    public int Id { get; set; }
-    public string FirstName { get; set; } = "";
-    public string LastName { get; set; } = "";
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string CardNumber { get; set; }
     public string? Email { get; set; }
     public string? Phone { get; set; }
+    public bool IsBanned  { get; set; } = false;
+    public DateTime? BannedDate { get; set; }
 
-    public string FullName => string.Format("{0} {1}", FirstName, LastName);
+    public virtual ICollection<Borrowing> Borrowings { get; set; } = [];
+
+    public string FullName => $"{FirstName} {LastName}".Trim();
 }
