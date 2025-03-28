de2353466215221264ec3ddc40f6b3497b449288
SzaroBurys <mkmateuszk@o2.pl>
2025-03-28 08:21:29 +0100

    Created a static class to facilitate sample data initialization for testing purposes.

---
+++ tests/Application.Tests/Helpers/DataInitializer.cs
@@ -0,0 +1,192 @@
+using SimpleLibrary.Application.Services.Abstraction;
+using SimpleLibrary.Domain.Enumerations;
+using SimpleLibrary.Domain.Models;
+using SimpleLibrary.Domain.Repositories;
+
+namespace SimpleLibrary.Tests.Application;
+
+public static class DataInitializer
+{
+    public static Dictionary<string, Guid> InitializeGuids()
+    {
+        return new Dictionary<string, Guid>
+        {
+            ["a1"] = Guid.NewGuid(),
+            ["a2"] = Guid.NewGuid(),
+            ["a3"] = Guid.NewGuid(),
+            ["c1"] = Guid.NewGuid(),
+            ["c2"] = Guid.NewGuid(),
+            ["b1"] = Guid.NewGuid(),
+            ["b2"] = Guid.NewGuid(),
+            ["b3"] = Guid.NewGuid(),
+            ["b4"] = Guid.NewGuid(),
+            ["b5"] = Guid.NewGuid(),
+            ["b6"] = Guid.NewGuid(),
+            ["b1_c1"] = Guid.NewGuid(),
+            ["b1_c2"] = Guid.NewGuid(),
+            ["b2_c3"] = Guid.NewGuid(),
+            ["b2_c4"] = Guid.NewGuid(),
+            ["b3_c5"] = Guid.NewGuid(),
+            ["b4_c6"] = Guid.NewGuid(),
+            ["r"] = Guid.NewGuid(),
+            ["bor1"] = Guid.NewGuid(),
+            ["bor2"] = Guid.NewGuid(),
+            ["bor3"] = Guid.NewGuid(),
+            ["bor4"] = Guid.NewGuid(),
+            ["bor5"] = Guid.NewGuid(),
+            ["bor6"] = Guid.NewGuid()
+        };
+    }
+
+    public static Mock<IRepository<Author>> InitializeAuthorRepository(Dictionary<string, Guid> guids)
+    {
+        Author a1 = new() { Id = guids["a1"], FirstName = "N/A", LastName = "N/A", BornDate = null };
+        Author a2 = new() { Id = guids["a2"], FirstName = "Adam", LastName = "Mickiewicz", BornDate = new DateTime(1798, 12, 24) };
+        Author a3 = new() { Id = guids["a3"], FirstName = "Jan", LastName = "Kowalski", BornDate = new DateTime(1968, 12, 09) };
+        List<Author> authors = [a1, a2, a3];
+
+        Mock<IRepository<Author>> mockAuthorRepository = new();
+        mockAuthorRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(authors);
+        mockAuthorRepository.Setup(repo => repo.GetByIdAsync(guids["a1"])).ReturnsAsync(a1);
+        mockAuthorRepository.Setup(repo => repo.GetByIdAsync(guids["a2"])).ReturnsAsync(a2);
+        mockAuthorRepository.Setup(repo => repo.GetByIdAsync(guids["a3"])).ReturnsAsync(a3);
+        return mockAuthorRepository;
+    }
+
+    public static Mock<IAuthorService> InitializeAuthorService(Dictionary<string, Guid> guids)
+    {
+        Author a1 = new() { Id = guids["a1"], FirstName = "N/A", LastName = "N/A", BornDate = null };
+        Author a2 = new() { Id = guids["a2"], FirstName = "Adam", LastName = "Mickiewicz", BornDate = new DateTime(1798, 12, 24) };
+        Author a3 = new() { Id = guids["a3"], FirstName = "Jan", LastName = "Kowalski", BornDate = new DateTime(1968, 12, 09) };
+        List<Author> authors = [a1, a2, a3];
+
+        Mock<IAuthorService> mockAuthorService = new();
+        mockAuthorService.Setup(repo => repo.GetAllAuthorsAsync()).ReturnsAsync(authors);
+        mockAuthorService.Setup(repo => repo.GetAuthorByIdAsync(guids["a1"])).ReturnsAsync(a1);
+        mockAuthorService.Setup(repo => repo.GetAuthorByIdAsync(guids["a2"])).ReturnsAsync(a2);
+        mockAuthorService.Setup(repo => repo.GetAuthorByIdAsync(guids["a3"])).ReturnsAsync(a3);
+        return mockAuthorService;
+    }
+
+    public static Mock<ICategoryRepository> InitializeCategories(Dictionary<string, Guid> guids)
+    {
+        Category c1 = new() { Id = guids["c1"], Name = "Novel" };
+        Category c2 = new() { Id = guids["c2"], Name = "Other" };
+        List<Category> categories = [c1, c2];
+
+        Mock<ICategoryRepository> mockCategoryRepository = new();
+        mockCategoryRepository.Setup(repo => repo.GetAllCategories()).Returns(categories);
+        mockCategoryRepository.Setup(repo => repo.GetCategories()).Returns(categories.AsQueryable());
+        mockCategoryRepository.Setup(repo => repo.GetCategory(guids["c1"])).Returns(c1);
+        mockCategoryRepository.Setup(repo => repo.GetCategory(guids["c2"])).Returns(c2);
+        return mockCategoryRepository;
+    }
+
+    public static async Task<Mock<IBookRepository>> InitializeBookRepository(
+        Dictionary<string, Guid> guids, 
+        Mock<IAuthorService> mockAuthorRepository, 
+        Mock<ICategoryRepository> mockCategoryRepository)
+    {
+        var a1 = await mockAuthorRepository.Object.GetAuthorByIdAsync(guids["a1"]);
+        var a2 = await mockAuthorRepository.Object.GetAuthorByIdAsync(guids["a1"]);
+        var a3 = await mockAuthorRepository.Object.GetAuthorByIdAsync(guids["a1"]);
+        Category c1 = mockCategoryRepository.Object.GetCategory(guids["c1"]) ?? throw new KeyNotFoundException("Category c1 not found.");
+        Category c2 = mockCategoryRepository.Object.GetCategory(guids["c2"]) ?? throw new KeyNotFoundException("Category c2 not found.");
+
+        Book b1 = new() { Id = guids["b1"], Title = "Some old book", Author = a1, AuthorId = guids["a1"], Category = c1, CategoryId = guids["c1"], Language = Language.English, ReleaseDate = new DateTime(1900, 1, 1) };
+        Book b2 = new() { Id = guids["b2"], Title = "Some old German book", Author = a1, AuthorId = guids["a1"], Category = c1, CategoryId = guids["c1"], Language = Language.German, ReleaseDate = new DateTime(1800, 1, 1) };
+        Book b3 = new() { Id = guids["b3"], Title = "Some new French book", Author = a1, AuthorId = guids["a1"], Category = c2, CategoryId = guids["c2"], Language = Language.French, ReleaseDate = new DateTime(2010, 5, 7) };
+        Book b4 = new() { Id = guids["b4"], Title = "Dziady część II", Author = a2, AuthorId = guids["a2"], Category = c2, CategoryId = guids["c2"], Language = Language.Polish, ReleaseDate = new DateTime(1823, 1, 1) };
+        Book b5 = new() { Id = guids["b5"], Title = "Dziady część III", Author = a2, AuthorId = guids["a2"], Category = c2, CategoryId = guids["c2"], Language = Language.Polish, ReleaseDate = new DateTime(1832, 1, 1) };
+        Book b6 = new() { Id = guids["b6"], Title = "Another book", Author = a3, AuthorId = guids["a3"], Category = c1, CategoryId = guids["c1"], Language = Language.English, ReleaseDate = new DateTime(1985, 1, 9), Tags = "Some" };
+        List<Book> books = [b1, b2, b3, b4, b5, b6];
+
+        Mock<IBookRepository> mockBookRepository = new();
+        mockBookRepository.Setup(repo => repo.GetAllBooks()).Returns(books);
+        mockBookRepository.Setup(repo => repo.GetBooks()).Returns(books.AsQueryable());
+        mockBookRepository.Setup(repo => repo.GetBook(guids["b1"])).Returns(b1);
+        mockBookRepository.Setup(repo => repo.GetBook(guids["b2"])).Returns(b2);
+        mockBookRepository.Setup(repo => repo.GetBook(guids["b3"])).Returns(b3);
+        mockBookRepository.Setup(repo => repo.GetBook(guids["b4"])).Returns(b4);
+        mockBookRepository.Setup(repo => repo.GetBook(guids["b5"])).Returns(b5);
+        mockBookRepository.Setup(repo => repo.GetBook(guids["b6"])).Returns(b6);
+
+        return mockBookRepository;
+    }
+
+    public static Mock<ICopyRepository> InitializeCopies(
+        Dictionary<string, Guid> guids, 
+        Mock<IBookRepository> mockBookRepository)
+    {
+        Book b1 = mockBookRepository.Object.GetBook(guids["b1"]) ?? throw new KeyNotFoundException("Book b1 not found.");
+        Book b2 = mockBookRepository.Object.GetBook(guids["b2"]) ?? throw new KeyNotFoundException("Book b2 not found.");
+        Book b3 = mockBookRepository.Object.GetBook(guids["b3"]) ?? throw new KeyNotFoundException("Book b3 not found.");
+        Book b4 = mockBookRepository.Object.GetBook(guids["b4"]) ?? throw new KeyNotFoundException("Book b4 not found.");
+
+        Copy c1 = new() { Id = guids["b1_c1"], Book = b1, BookId = guids["b1"] };
+        Copy c2 = new() { Id = guids["b1_c2"], Book = b1, BookId = guids["b1"] };
+        Copy c3 = new() { Id = guids["b2_c3"], Book = b2, BookId = guids["b2"] };
+        Copy c4 = new() { Id = guids["b2_c4"], Book = b2, BookId = guids["b2"] };
+        Copy c5 = new() { Id = guids["b3_c5"], Book = b3, BookId = guids["b3"] };
+        Copy c6 = new() { Id = guids["b4_c6"], Book = b4, BookId = guids["b4"] };
+        List<Copy> copies = [c1, c2, c3, c4, c5, c6];
+
+        Mock<ICopyRepository> mockCopyRepository = new();
+        mockCopyRepository.Setup(repo => repo.GetAllCopies()).Returns(copies);
+        mockCopyRepository.Setup(repo => repo.GetCopies()).Returns(copies.AsQueryable());
+        mockCopyRepository.Setup(repo => repo.GetCopy(guids["b1_c1"])).Returns(c1);
+        mockCopyRepository.Setup(repo => repo.GetCopy(guids["b1_c2"])).Returns(c2);
+        mockCopyRepository.Setup(repo => repo.GetCopy(guids["b2_c3"])).Returns(c3);
+        mockCopyRepository.Setup(repo => repo.GetCopy(guids["b2_c4"])).Returns(c4);
+        mockCopyRepository.Setup(repo => repo.GetCopy(guids["b3_c5"])).Returns(c5);
+        mockCopyRepository.Setup(repo => repo.GetCopy(guids["b4_c6"])).Returns(c6);
+
+        return mockCopyRepository;
+    }
+
+    public static Mock<IReaderRepository> InitializeReaders(Dictionary<string, Guid> guids)
+    {
+        Reader r = new() { Id = guids["r"], CardNumber = "000-111-222", FirstName = "Jan", LastName = "Kowalski", Email = "jan.kowalski@mail.com", Phone = "+48 789 456 123" };
+        List<Reader> readers = [r];
+
+        Mock<IReaderRepository> mockReaderRepository = new();
+        mockReaderRepository.Setup(repo => repo.GetAllReaders()).Returns(readers);
+        mockReaderRepository.Setup(repo => repo.GetReaders()).Returns(readers.AsQueryable());
+        mockReaderRepository.Setup(repo => repo.GetReader(guids["r"])).Returns(r);
+
+        return mockReaderRepository;
+    }
+
+    public static Mock<IBorrowingRepository> InitializeBorrowings(
+        Dictionary<string, Guid> guids, 
+        Mock<ICopyRepository> mockCopyRepository,
+        Mock<IReaderRepository> mockReaderRepository)
+    {
+        Copy c1 = mockCopyRepository.Object.GetCopy(guids["b1_c1"]) ?? throw new KeyNotFoundException("Copy c1 not found.");
+        Copy c2 = mockCopyRepository.Object.GetCopy(guids["b1_c2"]) ?? throw new KeyNotFoundException("Copy c2 not found.");
+        Copy c3 = mockCopyRepository.Object.GetCopy(guids["b2_c3"]) ?? throw new KeyNotFoundException("Copy c3 not found.");
+        Copy c5 = mockCopyRepository.Object.GetCopy(guids["b3_c5"]) ?? throw new KeyNotFoundException("Copy c5 not found.");
+        Copy c6 = mockCopyRepository.Object.GetCopy(guids["b4_c6"]) ?? throw new KeyNotFoundException("Copy c6 not found.");
+        Reader r1 = mockReaderRepository.Object.GetReader(guids["r"]) ?? throw new KeyNotFoundException("Reader r1 not found.");
+
+        Borrowing bor1 = new() { Id = guids["bor1"], Copy = c1, CopyId = guids["b1_c1"], Reader = r1, ReaderId = guids["r"], StartedDate = DateTime.Now.AddDays(-5) };
+        Borrowing bor2 = new() { Id = guids["bor2"], Copy = c2, CopyId = guids["b1_c2"], Reader = r1, ReaderId = guids["r"], StartedDate = DateTime.Now.AddDays(-5) };
+        Borrowing bor3 = new() { Id = guids["bor3"], Copy = c3, CopyId = guids["b2_c3"], Reader = r1, ReaderId = guids["r"], StartedDate = DateTime.Now.AddDays(-5) };
+        Borrowing bor4 = new() { Id = guids["bor4"], Copy = c5, CopyId = guids["b3_c5"], Reader = r1, ReaderId = guids["r"], StartedDate = DateTime.Now.AddDays(-5), ActualReturnDate = DateTime.Now.AddDays(-2) };
+        Borrowing bor5 = new() { Id = guids["bor5"], Copy = c6, CopyId = guids["b4_c6"], Reader = r1, ReaderId = guids["r"], StartedDate = DateTime.Now.AddDays(-15), ActualReturnDate = DateTime.Now.AddDays(-10) };
+        Borrowing bor6 = new() { Id = guids["bor6"], Copy = c6, CopyId = guids["b4_c6"], Reader = r1, ReaderId = guids["r"], StartedDate = DateTime.Now.AddDays(-5) };
+        List<Borrowing> borrowings = [bor1, bor2, bor3, bor4, bor5, bor6];
+
+        Mock<IBorrowingRepository> mockBorrowingRepository = new();
+        mockBorrowingRepository.Setup(repo => repo.GetAllBorrowings()).Returns(borrowings);
+        mockBorrowingRepository.Setup(repo => repo.GetBorrowings()).Returns(borrowings.AsQueryable());
+        mockBorrowingRepository.Setup(repo => repo.GetBorrowing(guids["bor1"])).Returns(bor1);
+        mockBorrowingRepository.Setup(repo => repo.GetBorrowing(guids["bor2"])).Returns(bor2);
+        mockBorrowingRepository.Setup(repo => repo.GetBorrowing(guids["bor3"])).Returns(bor3);
+        mockBorrowingRepository.Setup(repo => repo.GetBorrowing(guids["bor4"])).Returns(bor4);
+        mockBorrowingRepository.Setup(repo => repo.GetBorrowing(guids["bor5"])).Returns(bor5);
+        mockBorrowingRepository.Setup(repo => repo.GetBorrowing(guids["bor6"])).Returns(bor6);
+
+        return mockBorrowingRepository;
+    }
+}
