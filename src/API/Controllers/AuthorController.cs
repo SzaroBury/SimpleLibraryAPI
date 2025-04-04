b5cdc942f126faaaf4f3ac8b613c6a6e09b84dca
SzaroBurys <mkmateuszk@o2.pl>
2025-04-04 15:15:31 +0200

    Added exception handling to the AuthorController.

--- src/API/Controllers/AuthorController.cs
+++ src/API/Controllers/AuthorController.cs
@@ -1,108 +1,175 @@
 ﻿using SimpleLibrary.API.Attributes;
-using SimpleLibrary.Domain.Models;
-using SimpleLibrary.Domain.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using SimpleLibrary.Application.Services.Abstraction;
+using SimpleLibrary.Domain.DTO;
 
 namespace SimpleLibrary.API.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/authors")]
 [ApiController]
 public class AuthorController : ControllerBase
 {
-    private readonly IAuthorRepository authorRepository;
     private readonly IAuthorService authorService;
     private readonly ILogger<AuthorController> logger;
-    public AuthorController(IAuthorRepository authorRepository, IAuthorService authorService, ILogger<AuthorController> logger)
+    public AuthorController(IAuthorService authorService, ILogger<AuthorController> logger)
     {
-        this.authorRepository = authorRepository;
         this.authorService = authorService;
         this.logger = logger;
     }
 
-    // GET: api/authors
-    [HttpGet("~/api/Authors")]
+    [HttpGet("search")]
     [ApiKey("ReadOnly", "Librarian", "Admin")]
-    public IActionResult GetAll()
+    public async Task<IActionResult> GetAll(
+        [FromQuery] string? search = null, 
+        [FromQuery] string? olderThan = null, 
+        [FromQuery] string? youngerThan = null, 
+        [FromQuery] int? page = null, 
+        [FromQuery] int? pageSize = null
+    )
     {
         try
         {
             logger.LogInformation("GetAll request received.");
-            var result = authorRepository.GetAllAuthors();
+            var result = await authorService.SearchAuthorsAsync(search, olderThan, youngerThan, page ?? 1, pageSize ?? 25);
             return Ok(result);
+        }
+        catch(ArgumentException e)
+        {
+            logger.LogInformation($"ArgumentException catched during invoking GetAll(search: {search}, olderThan: {olderThan}, youngerThan: {youngerThan}, page: {page ?? 1}, pageSize: {pageSize ?? 25}):");
+            logger.LogInformation($"    {e.Message}");
+            return ValidationProblem(e.Message);
+        }
+        catch(FormatException e)
+        {
+            logger.LogInformation($"FormatException catched during invoking GetAll(search: {search}, olderThan: {olderThan}, youngerThan: {youngerThan}, page: {page ?? 1}, pageSize: {pageSize ?? 25}):");
+            logger.LogInformation($"    {e.Message}");
+            return ValidationProblem(e.Message);
         }
         catch (Exception e)
         {
-            return StatusCode(500, $"Error: {e.Message}");
+            logger.LogError($"{DateTime.Now}: Unexpected error during invoking GetAll(search: {search}, olderThan: {olderThan}, youngerThan: {youngerThan}, page: {page ?? 1}, pageSize: {pageSize ?? 25}):");
+            logger.LogError($"    {e.Message}");
+            return StatusCode(500, $"Unexpected error.");
         }
     }
 
-    // GET api/author/5
     [HttpGet("{id}")]
     [ApiKey("ReadOnly", "Librarian", "Admin")]
-    public IActionResult Get(string id)
+    public async Task<IActionResult> Get(string id)
     {
         try
         {
             logger.LogInformation("Get request received.");
-            var result = authorRepository.GetAuthor(Guid.Parse(id));
+            var result = await authorService.GetAuthorByIdAsync(id);
             return Ok(result);
+        }
+        catch(FormatException e)
+        {
+            logger.LogInformation($"FormatException catched during invoking Get(id: {id}):");
+            logger.LogInformation($"    {e.Message}");
+            return ValidationProblem(e.Message);
+        }
+        catch(KeyNotFoundException e)
+        {
+            logger.LogInformation($"KeyNotFoundException catched during invoking Get(id: {id}):");
+            logger.LogInformation($"    {e.Message}");
+            return NotFound(e.Message);
         }
         catch (Exception e)
         {
-            return StatusCode(500, $"Error: {e.Message}");
+            logger.LogError($"{DateTime.Now}: Unexpected error during invoking Get(id: {id}):");
+            logger.LogError($"    {e.Message}");
+            return StatusCode(500, $"Unexpected error.");
         }
     }
 
-    // POST api/authors
     [HttpPost]
     [ApiKey("Librarian", "Admin")]
-    public IActionResult Post(Author author)
+    public async Task<IActionResult> Post(AuthorPostDTO author)
     {
         try
         {
             logger.LogInformation("Post request received.");
-            authorRepository.CreateAuthor(author);
+            await authorService.CreateAuthorAsync(author);
             return Ok("Object was sucesfully added to the datebase.");
+        }
+        catch(ArgumentException e)
+        {
+            logger.LogInformation($"ArgumentException catched during invoking Post(<AuthorPostDTO Object>):");
+            logger.LogInformation($"    {e.Message}");
+            return ValidationProblem(e.Message);
+        }
+        catch(FormatException e)
+        {
+            logger.LogInformation($"FormatException catched during invoking Post(<AuthorPostDTO Object>):");
+            logger.LogInformation($"    {e.Message}");
+            return ValidationProblem(e.Message);
         }
         catch (Exception e)
         {
-            return StatusCode(500, $"Error: {e.Message}");
+            logger.LogError($"{DateTime.Now}: Unexpected error during invoking Post(<AuthorPostDTO Object>):");
+            logger.LogError($"    {e.Message}");
+            return StatusCode(500, $"Unexpected error.");
         }
     }
 
-    // PUT api/authors
-    [HttpPut("{id}")]
+    [HttpPut]
     [ApiKey("Librarian", "Admin")]
-    public IActionResult Put(string id, Author author)
+    public async Task<IActionResult> Put(AuthorPutDTO author)
     {
         try
         {
             logger.LogInformation("Put request received.");
-            author.Id = Guid.Parse(id);
-            authorRepository.UpdateAuthor(author);
+            await authorService.UpdateAuthorAsync(author);
             return Ok("Object was sucesfully updated in the datebase.");
+        }
+        catch(FormatException e)
+        {
+            logger.LogInformation($"FormatException catched during invoking Put(<AuthorPutDTO Object, Id: {author.Id}>):");
+            logger.LogInformation($"    {e.Message}");
+            return ValidationProblem(e.Message);
+        }
+        catch(KeyNotFoundException e)
+        {
+            logger.LogInformation($"KeyNotFoundException catched during invoking Put(<AuthorPutDTO Object, Id: {author.Id}>):");
+            logger.LogInformation($"    {e.Message}");
+            return NotFound(e.Message);
         }
         catch (Exception e)
         {
-            return StatusCode(500, $"Error: {e.Message}");
+            logger.LogError($"{DateTime.Now}: Unexpected error during invoking Put(<AuthorPutDTO Object - id: {author.Id}>):");
+            logger.LogError($"    {e.Message}");
+            return StatusCode(500, $"Unexpected error.");
         }
     }
 
-    // DELETE api/authors/5
     [HttpDelete("{id}")]
     [ApiKey("Admin")]
-    public IActionResult Delete(string id)
+    public async Task<IActionResult> Delete(string id)
     {
         try
         {
             logger.LogInformation("Delete request received.");
-            authorRepository.DeleteAuthor(Guid.Parse(id));
-            return StatusCode(200, "Object was sucesfully deleted from the datebase.");
+            await authorService.DeleteAuthorAsync(id);
+            return Ok("Object was sucesfully deleted from the datebase.");
+        }
+        catch(FormatException e)
+        {
+            logger.LogInformation($"FormatException catched during invoking Delete(id: {id}):");
+            logger.LogInformation($"    {e.Message}");
+            return ValidationProblem(e.Message);
+        }
+        catch(KeyNotFoundException e)
+        {
+            logger.LogInformation($"KeyNotFoundException catched during invoking Delete(id: {id}):");
+            logger.LogInformation($"    {e.Message}");
+            return NotFound(e.Message);
         }
         catch (Exception e)
         {
-            return StatusCode(500, $"Error: {e.Message}");
+            logger.LogError($"{DateTime.Now}: Unexpected error during invoking Delete(id: {id}):");
+            logger.LogError($"    {e.Message}");
+            return StatusCode(500, $"Unexpected error.");
         }
     }
 }
